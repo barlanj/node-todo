bd4cfefeef20ec3e55bcc09948b262eb0bfd92cb
barlanj <barlanj@oregonstate.edu>
2016-07-07 18:00:53 -0400

    added POST

--- server.js
+++ server.js
@@ -1,32 +1,42 @@
 var express = require('express');
+var bodyParser = require('body-parser');
+var _ = require('underscore');
+
 var app = express();
 var PORT = process.env.PORT || 3000;
+var uniqueID = 0;
 
 var todos = [];
 
+
+app.use(bodyParser.json());
+
+//--------------------------------------------------------
 function TodoItem (key, desc, done) {
 	this.id = key;
 	this.description = desc;
 	this.complete = done;
 }
 
-for (var i = 0; i < 3; i++) {
+for (var i = uniqueID; i < 3; i++) {
 	var task = new TodoItem(i, 'task_' + i, false);
 	todos.push(task);
+	uniqueID++;
 }
 
 
 function getTodoItem(key, arr) {
-	for (var i = arr.length - 1; i >= 0; i--) {
-		if (arr[i].id == key) {
-			return arr[i];
-		}
-	}
 
-	return null;
+	return _.findWhere(arr, {id: Number(key)});
 }
 
+function addTodoItem(item) {
 
+	item.id = todos.length;
+	todos.push(item);
+}
+
+//-----------------------------------------------------------
 app.get('/', function (req, res) {
 	 res.send('root'); 
 });
@@ -36,17 +46,33 @@ app.get('/todos', function (req, res) {
 });
 
 app.get('/todos/:id', function (req, res) {
-	 var todo = getTodoItem(req.params.id, todos);
+	 var fetchedItem = getTodoItem(req.params.id, todos);
 
-	 if(!todo) {
+	 if(!fetchedItem) {
 	 	res.status(404).send();
 	 } else {
-	 	res.json([todo]);
+	 	res.json([fetchedItem]);
 	 }
 
 });
 
+app.post('/todos', function (req, res) {
+	 var body = _.pick(req.body, 'description', 'complete');
 
+	 if (!_.isBoolean(body.complete) || !_.isString(body.description) 
+	 	|| body.description.trim().length === 0) {
+
+	 	return res.status(400).send();
+	 }
+
+	 body.description = body.description.trim();
+	 addTodoItem(body);
+	 
+	 res.json(todos);
+	 
+});
+
+//------------------------------------------------------------
 app.listen(PORT, function () {
 	 console.log('listening ...');
 });
